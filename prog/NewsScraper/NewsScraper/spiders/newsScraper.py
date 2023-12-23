5da15888b76a618e6d9d2effb43b21e5663c7182
mabk748 <mabk748@gmail.com>
2023-12-23 12:07:09 +0100

    Working on the spider

--- prog/NewsScraper/NewsScraper/spiders/newsScraper.py
+++ prog/NewsScraper/NewsScraper/spiders/newsScraper.py
@@ -4,7 +4,14 @@ import scrapy
 class NewsscraperSpider(scrapy.Spider):
     name = "newsScraper"
     allowed_domains = ["news.google.com"]
-    start_urls = ["https://news.google.com"]
+    start_urls = ["https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx1YlY4U0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen"]
 
     def parse(self, response):
-        pass
+        
+        articles = response.xpath("//c-wiz/c-wiz/c-wiz")
+        for article in articles:
+            #title = article.xpath("/c-wiz/c-wiz/article/a/text()")
+            yield {
+                "title": article.css("article a::text").get(),
+                "link": article.css("article a").attrib["href"]
+                }
